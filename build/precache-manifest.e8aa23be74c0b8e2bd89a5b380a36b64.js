4784d8da11d55e09d33956f5fccc5a4f6ce81526
Rebeca Nonato Silva <38442994+REBECANONATO@users.noreply.github.com>
2020-01-18 14:20:41 -0300

    build

---
+++ build/precache-manifest.e8aa23be74c0b8e2bd89a5b380a36b64.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b986852ccfc7281f140a68d4b0a0f347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7226b5cb69f985b7e43c",
+    "url": "/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "3d1e498458d751ddeaae",
+    "url": "/static/css/main.93b1b5a5.chunk.css"
+  },
+  {
+    "revision": "7226b5cb69f985b7e43c",
+    "url": "/static/js/2.5f82942b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f82942b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d1e498458d751ddeaae",
+    "url": "/static/js/main.2f9462b5.chunk.js"
+  },
+  {
+    "revision": "861e6ff718f239871ec1",
+    "url": "/static/js/runtime-main.4a260cd7.js"
+  },
+  {
+    "revision": "e445f7b4e94b644adeefa6b7feeed3e0",
+    "url": "/static/media/about-us.e445f7b4.svg"
+  },
+  {
+    "revision": "c2bdbf1b29e142c39206daf6c2ad04d2",
+    "url": "/static/media/andre-oliveira.c2bdbf1b.png"
+  },
+  {
+    "revision": "c47cc6bb2b6ed001aad23b5b6b69b503",
+    "url": "/static/media/derickson-loss.c47cc6bb.png"
+  },
+  {
+    "revision": "2786f13255b20d5839845d8b5713366b",
+    "url": "/static/media/icon-desktop.2786f132.svg"
+  },
+  {
+    "revision": "693aa536feb7c15df1916849bd003b24",
+    "url": "/static/media/icon-mobile.693aa536.svg"
+  },
+  {
+    "revision": "76132631d457f13c9c26eaf6fbfad237",
+    "url": "/static/media/icon-mouse-scroll.76132631.svg"
+  },
+  {
+    "revision": "8409590c355ff1149a458a55e4bf3753",
+    "url": "/static/media/logo.8409590c.svg"
+  },
+  {
+    "revision": "22dff533200963a2ca2d9b59a3d216dc",
+    "url": "/static/media/lucas-t.22dff533.png"
+  },
+  {
+    "revision": "c22c3c5a3c26ab7f3ecedc8d23192113",
+    "url": "/static/media/mickael-dantas.c22c3c5a.png"
+  },
+  {
+    "revision": "362fa82753fa2fb9015cbfadd8d7989b",
+    "url": "/static/media/projects-paper.362fa827.svg"
+  },
+  {
+    "revision": "9e93739257c034d8ce5241248ded1a67",
+    "url": "/static/media/rebeca-nonato.9e937392.png"
+  },
+  {
+    "revision": "a14dece072db3c50e2a4ebf0618ee2cb",
+    "url": "/static/media/wander-hungerbuhler.a14dece0.png"
+  }
+]);
